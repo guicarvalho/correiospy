c0e174801f3c5566ed7e844c44b2fffc0e24c880
Guilherme Carvalho <gui.ifsp11@gmail.com>
2015-02-16 17:32:54 -0200

    Change module name.

---
+++ correiospy/config.py
@@ -0,0 +1,5 @@
+# coding: utf-8
+
+URL_WS_CORREIOS = 'http://ws.correios.com.br/calculador/CalcPrecoPrazo.asmx'
+
+NOT_SERVICE_VALUE = 'N'
--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
-setup(name='pycorreios',
+setup(name='correiospy',
       version='0.1',
-      description='Calculates the amountof supplies through zip code. The values are found at WS the Correios in Brazil.',
+      description='Calculates the amount of supplies through zip code. The values are found at WS the Correios in Brazil.',
       url='http://github.com/guicarvalho/pycorreios',
       author='Guilherme Arthur de Carvalho',
       author_email='gui.ifsp11@gmail.com',
       license='MIT',
-      packages=['pycorreios'],
+      packages=['correiospy'],
       zip_safe=False
       )
